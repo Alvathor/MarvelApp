2b2513f90db5a580553e4c6323e0ce63c058edc2
Juliano <alvarenga.vix@gmail.com>
2021-02-13 19:36:48 -0300

    Initial Commit

---
+++ Shared/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  Shared
+//
+//  Created by Juliano Alvarenga on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
